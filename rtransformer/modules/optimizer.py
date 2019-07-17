2936258e399d25a7bbdf29b17e6eb08eec3b6ab4
novdov <rpwjd56@gmail.com>
2019-07-17 22:08:40 +0900

    inherit baseclass of torch

--- rtransformer/modules/optimizer.py
+++ rtransformer/modules/optimizer.py
@@ -1,12 +1,13 @@
 import torch
 
 
-class TransformerLRScheduler:
+class TransformerLRScheduler(torch.optim.lr_scheduler._LRScheduler):
     """Learning rate scheduler used in standard transformer."""
 
     def __init__(
         self, optimizer: torch.optim.Adam, d_model: int, warmup_steps: int = 4000
     ):
+        super().__init__(optimizer)
         self.optimizer = optimizer
         self.d_model = d_model
         self.warmup_steps = warmup_steps
@@ -14,10 +15,8 @@ class TransformerLRScheduler:
 
         self._step = 0
 
-    def step(self):
-        self._step += 1
-
     def get_lr(self):
+        self._step += 1
         arg1 = self.d_model ** -0.5
         arg2 = self._step * (self.warmup_steps ** -1.5)
         return self.init_lr * min(arg1, arg2)
